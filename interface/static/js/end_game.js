5cc0de9dba20b818f12d1931dc2b99b630f82c9f
Francisco Querido <148355011+FranciscoQuerido03@users.noreply.github.com>
2024-11-29 00:54:43 +0000

    Small improve to the improve of  the win/lose page

--- interface/static/js/end_game.js
+++ interface/static/js/end_game.js
@@ -4,9 +4,39 @@ function victory() {
     const win_sound = new Audio(win);
     win_sound.play();
     confetti();
+
+    // Seleciona o elemento com a classe "Vitória"
+    const elementoTexto = document.querySelector(".Vitória");
+    if (elementoTexto) {
+        // Adiciona a classe para animação
+        elementoTexto.classList.add("vitoria-animacao");
+
+        // Remove a classe após a animação para permitir reuso
+        setTimeout(() => {
+            elementoTexto.classList.remove("vitoria-animacao");
+        }, 2000); // Duração da animação (2 segundos)
+    }
 }
 
 function Lose() {
+    const elementoTexto = document.querySelector(".Derrota"); 
+    
+    if (elementoTexto) {
+        const textoCompleto = elementoTexto.textContent + "..."; 
+        let indice = 0;
+        elementoTexto.textContent = ""; 
+
+        function escreverTexto() {
+            if (indice < textoCompleto.length) {
+                elementoTexto.textContent += textoCompleto.charAt(indice);
+                indice++;
+                setTimeout(escreverTexto, 100);
+            }
+        }
+
+        escreverTexto(); 
+    }
+
     const soundsDiv = document.getElementById("sounds");
     const lose = soundsDiv.getAttribute("lose-sound");
     const lose_sound = new Audio(lose);
