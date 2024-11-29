9fbebd65c5e7df6f06831c5c2169f47d1003b96b
Francisco Querido <148355011+FranciscoQuerido03@users.noreply.github.com>
2024-11-29 00:40:54 +0000

    - Lose and Win little improve - Logic so a player cant get a second room that has the answer in the first one.

--- interface/views.py
+++ interface/views.py
@@ -330,7 +330,7 @@ def check_answer(request):
             room.save()
 
             # Encontrar a próxima sala disponível
-            next_room = Room.objects.filter(perms=True, ocupied=False, final=False).first()
+            next_room = Room.objects.filter(perms=True, ocupied=False, final=False).exclude(name=room.answers_to.name).first()
 
         # Atualizar a próxima sala para ocupada
         if next_room:
